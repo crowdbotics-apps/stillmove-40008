b40086a4930b2602075069102a37a617e6627afd
Crowdbotics <team@crowdbotics.com>
2023-04-18 17:03:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "stillmove-40008",
+  url: "https://stillmove-40008.botics.co",
+  api: "https://stillmove-40008.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
